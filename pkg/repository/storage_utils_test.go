9add6990fd06560a34ed9a56241b7f4f287a8940
Chester Asuncion <asuncionmcv@gmail.com>
2022-07-06 22:15:07 +0800

    add tests for asserting database config file existence

--- pkg/repository/storage_utils_test.go
+++ pkg/repository/storage_utils_test.go
@@ -1,6 +1,7 @@
 package repository_test
 
 import (
+	"errors"
 	"online-bidding-system/pkg/repository"
 	"testing"
 )
@@ -24,7 +25,7 @@ func TestAssertDatabaseConfigEnvExists(t *testing.T) {
 		{
 			name:     "Returns no error when given non existent environment",
 			env:      "missingEnv",
-			want_err: repository.ErrNonExistentDatabaseEnvironment,
+			want_err: &repository.ErrNonExistentDatabaseEnvironment{Env: "missingEnv"},
 		},
 	}
 
@@ -32,7 +33,46 @@ func TestAssertDatabaseConfigEnvExists(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			err := repository.AssertDatabaseConfigEnvExists(test.env)
 
-			if err != nil {
+			if !errors.Is(err, test.want_err) {
+				t.Errorf(
+					"Test failed: %v \n\tgot: %v\n\twant: %v",
+					test.name,
+					err,
+					test.want_err,
+				)
+			}
+		})
+	}
+}
+
+func TestAssertConfigFileExists(t *testing.T) {
+	tests := []struct {
+		name      string
+		file_path string
+		want_err  error
+	}{
+		{
+			name:      "Returns no error when given an existing database config file v1",
+			file_path: "/home/chester/Documents/code_projects/go-projects/go-online-bidding-system/pkg/repository/configurations/obs_test.json",
+			want_err:  nil,
+		},
+		{
+			name:      "Returns no error when given an existing database config file v2",
+			file_path: "/home/chester/Documents/code_projects/go-projects/go-online-bidding-system/pkg/repository/configurations/obs_dev.json",
+			want_err:  nil,
+		},
+		{
+			name:      "Returns an error when given an non existing filepath",
+			file_path: "/home/chester/Documents/code_projects/go-projects/go-online-bidding-system/pkg/repository/configurations/obs_nonExisting.json",
+			want_err:  &repository.ErrNonExistentConfigFile{Filepath: "/home/chester/Documents/code_projects/go-projects/go-online-bidding-system/pkg/repository/configurations/obs_nonExisting.json"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := repository.AssertConfigFileExists(test.file_path)
+
+			if !errors.Is(err, test.want_err) {
 				t.Errorf(
 					"Test failed: %v \n\tgot: %v\n\twant: %v",
 					test.name,
