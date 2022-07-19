72c6506b4667a6b3e462b26ea471051e323a4796
Chester Asuncion <asuncionmcv@gmail.com>
2022-07-19 19:32:17 +0800

    add UserRepository interface

--- pkg/api/repository_definitions.go
+++ pkg/api/repository_definitions.go
@@ -1,8 +1,15 @@
 package api
 
+type UserRepository interface {
+	AllUsers() (users []User, err error)
+	GetUser(uId int) (user []User, err error)
+	DeleteUser(uID int) (err error)
+	UpdateUser(user User) (err error)
+}
+
 type BidRepository interface {
 	AllBiddings() (biddings []Bidding, err error)
 	GetBidding(bID int) (bid Bidding, err error)
 	DeleteBidding(bID int) (err error)
-	UpdateBidding(bID int) (err error)
+	UpdateBidding(bid Bid) (err error)
 }
