3c7e9be2cb712a658745c3ad1078bafb703c13b2
Chester Asuncion <asuncionmcv@gmail.com>
2022-07-21 14:23:22 +0800

    add test for user handler AllUsers

--- pkg/app/user_handlers_test.go
+++ pkg/app/user_handlers_test.go
@@ -1,6 +1,9 @@
 package app_test
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"online-bidding-system/pkg/api"
 	"online-bidding-system/pkg/app"
 	"os"
@@ -8,7 +11,14 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
 )
+
+type User struct {
+	Name           string `json:"name"`
+	Email          string `json:"email"`
+	HashedPassword string `json:"hashedPass"`
+}
 
 var server *app.Server
 var router *gin.Engine
@@ -29,4 +39,34 @@ func TestMain(m *testing.M) {
 	// run the tests
 	exitValue := m.Run()
 	os.Exit(exitValue)
+}
+
+func TestAllUsers(t *testing.T) {
+	router.GET(`/v1/api/status`, server.AllUsers())
+	req, _ := http.NewRequest(`GET`, `/v1/api/status`, nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	user1 := User{
+		Name:           "User One",
+		Email:          "user1@email.com",
+		HashedPassword: "x120asd",
+	}
+
+	user2 := User{
+		Name:           "User Two",
+		Email:          "user2@email.com",
+		HashedPassword: "y562ash",
+	}
+
+	var response *app.GenericResponse
+	expected_response := &app.GenericResponse{
+		Status:  true,
+		Message: `Users successfully retrieved`,
+		Data:    []User{user1, user2},
+	}
+
+	json.Unmarshal(w.Body.Bytes(), &response)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, expected_response, response)
 }
