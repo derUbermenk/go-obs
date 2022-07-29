d927437b12d7277c26150415066d8fa3c03a8847
Chester Asuncion <asuncionmcv@gmail.com>
2022-07-29 10:46:49 +0800

    move mock service definitions to mockServiceDefinitions_test.go

--- pkg/app/user_handlers_test.go
+++ pkg/app/user_handlers_test.go
@@ -11,43 +11,6 @@ import (
 	"github.com/go-playground/assert/v2"
 )
 
-// this must be the same as that defined in api definition of the user.
-// this is made so that no api code is called in this test
-type mockUserService struct {
-	userRepo map[int]api.User
-}
-
-var userRepo = map[int]api.User{
-	0: api.User{
-		Name:           "User One",
-		Email:          "user1@email.com",
-		HashedPassword: "x120asd",
-	},
-	1: api.User{
-		Name:           "User Two",
-		Email:          "user2@email.com",
-		HashedPassword: "y562ash",
-	},
-}
-
-func (mU *mockUserService) All() ([]api.User, error) {
-	// return an array of all the users
-	return []api.User{mU.userRepo[0], mU.userRepo[1]}, nil
-}
-
-func (mU *mockUserService) Get(userID int) (api.User, error) {
-	return mU.userRepo[userID], nil
-}
-
-func (mU *mockUserService) Delete(userID int) error {
-	// simulate a successful delete operation
-	return nil
-}
-
-func (mU *mockUserService) Update(user api.User) error {
-	return nil
-}
-
 // sets up server to use mock user service
 // for all user handler tests
 func SetUpUserHandlersTest() {
