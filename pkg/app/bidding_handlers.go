78c89936b5410c2fc8d8122a7a9c28f075f0bfdc
Chester Asuncion <asuncionmcv@gmail.com>
2022-08-05 18:50:15 +0800

    Implement GetBidding

--- pkg/app/bidding_handlers.go
+++ pkg/app/bidding_handlers.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"errors"
 	"log"
 	"net/http"
+	"online-bidding-system/pkg/api"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,6 +33,60 @@ func (s *Server) AllBiddings() gin.HandlerFunc {
 				Status:  true,
 				Message: "Biddings successfully retrieved",
 				Data:    biddings,
+			},
+		)
+	}
+}
+
+func (s *Server) GetBidding() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var id int
+		var bidding api.Bidding
+		var err error
+		id, err = strconv.Atoi(c.Param(`id`))
+
+		if err != nil {
+			log.Printf("Handler Error: %v", err)
+			c.JSON(
+				http.StatusBadRequest,
+				&GenericResponse{
+					Status:  false,
+					Message: "Unable to get bidding",
+				},
+			)
+			return
+		}
+
+		bidding, err = s.bidding_service.Get(id)
+
+		if errors.Is(err, &api.ErrNonExistentUser{}) {
+			log.Printf("Handler Error: %v", err)
+			c.JSON(
+				http.StatusNotFound,
+				&GenericResponse{
+					Status:  false,
+					Message: "Bidding does not exist",
+				},
+			)
+			return
+		} else if err != nil {
+			log.Printf("Handler Error: %v", err)
+			c.JSON(
+				http.StatusBadRequest,
+				&GenericResponse{
+					Status:  false,
+					Message: "Unable to get bidding",
+				},
+			)
+			return
+		}
+
+		c.JSON(
+			http.StatusFound,
+			&GenericResponse{
+				Status:  true,
+				Message: "Bidding retrieved",
+				Data:    bidding,
 			},
 		)
 	}
