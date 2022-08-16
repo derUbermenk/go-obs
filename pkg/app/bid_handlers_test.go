bd0c7e6c1c1f6256dd57e84564c5ae35a40202f7
Chester Asuncion <asuncionmcv@gmail.com>
2022-08-16 16:30:43 +0800

    Implement case 1 for TestCreateBid
    case 1: amount is lower than lowest allowabl bid or current top bid
    amount

--- pkg/app/bid_handlers_test.go
+++ pkg/app/bid_handlers_test.go
@@ -1,9 +1,11 @@
 package app_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"online-bidding-system/pkg/api"
 	"online-bidding-system/pkg/app"
 	"testing"
 
@@ -33,9 +35,32 @@ func TestCreateBid(t *testing.T) {
 
 	define_route(`POST`, `/bids`, server.CreateBid())
 
-	request = initialize_request(`POST`, `/bids`, nil)
-	recorder = send_request(request)
+	// case 1: amount is lower than lowest allowable bid
+	t.Run(
+		`Amount is lower than lowest allowable bid`,
+		func(t *testing.T) {
 
-	json.Unmarshal(recorder.Body.Bytes(), &response)
-	assert.Equal(t, http.StatusOK, recorder.Code)
+			createBidRequest := &api.CreateBidRequest{
+				ID:        1,
+				BidderID:  1,
+				BiddingId: 1,
+				Amount:    100,
+			}
+			jsonValue, _ := json.Marshal(createBidRequest)
+
+			request = initialize_request(`POST`, `/bids`, bytes.NewBuffer(jsonValue))
+			recorder = send_request(request)
+
+			expected_response = &app.GenericResponse{
+				Status:  false,
+				Message: `The bid amount is lower than lowest allowable bid`,
+			}
+
+			json.Unmarshal(recorder.Body.Bytes(), &response)
+			assert.Equal(t, http.StatusConflict, recorder.Code)
+			assert.Equal(t, response.Status, expected_response.Status)
+			assert.Equal(t, response.Message, expected_response.Message)
+			assert.Equal(t, response.Data, expected_response.Data)
+		},
+	)
 }
