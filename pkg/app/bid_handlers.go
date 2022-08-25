0dc13accf9594f343769e412e9d5352838452369
Chester Asuncion <asuncionmcv@gmail.com>
2022-08-25 19:29:47 +0800

    implement CreateBidHandler

---
+++ pkg/app/bid_handlers.go
@@ -0,0 +1,49 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) CreateBid() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// get request body
+		// parse request body to &api.CreateBidRequest
+		err := c.ShouldBindJSON(createBidRequest)
+		if err != nil {
+			c.JSON(
+				http.StatusBadRequest,
+				&GenericResponse{
+					Status:  false,
+					Message: err.Error(),
+				},
+			)
+
+			return
+		}
+
+		// call s.bid_service.CreateBid()
+		id, err := s.bid_service.CreateBid(createBidRequest)
+		if err != nil {
+			c.JSON(
+				http.StatusInternalServerError,
+				&GenericResponse{
+					Status:  false,
+					Message: err.Error(),
+				},
+			)
+			return
+		}
+
+		// return
+		c.JSON(
+			http.StatusCreated,
+			&GenericResponse{
+				Status:  true,
+				Message: `Bid created`,
+				Data:    id,
+			},
+		)
+	}
+}
