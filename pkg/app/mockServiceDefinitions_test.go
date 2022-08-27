860ba57b77f8c89fbf13df54ec3b823a05bf5762
Chester Asuncion <asuncionmcv@gmail.com>
2022-08-27 19:00:59 +0800

    move mockUserService to user_handlers_test

--- pkg/app/mockServiceDefinitions_test.go
+++ pkg/app/mockServiceDefinitions_test.go
@@ -7,58 +7,6 @@ import (
 
 // this must be the same as that defined in api definition of the user.
 // this is made so that no api code is called in this test
-type mockUserService struct {
-	userRepo map[int]api.User
-}
-
-var userRepo = map[int]api.User{
-	0: {
-		Name:           "User One",
-		Email:          "user1@email.com",
-		HashedPassword: "x120asd",
-	},
-	1: {
-		Name:           "User Two",
-		Email:          "user2@email.com",
-		HashedPassword: "y562ash",
-	},
-}
-
-func (mU *mockUserService) All() ([]api.User, error) {
-	// return an array of all the users
-	return []api.User{mU.userRepo[0], mU.userRepo[1]}, nil
-}
-
-func (mU *mockUserService) Get(userID int) (api.User, error) {
-	user, exists := mU.userRepo[userID]
-
-	if !exists {
-		return user, &api.ErrNonExistentResource{}
-	}
-
-	return user, nil
-}
-
-func (mU *mockUserService) Delete(userID int) error {
-	// simulate a successful delete operation
-	_, exists := mU.userRepo[userID]
-
-	if !exists {
-		return &api.ErrNonExistentResource{}
-	}
-
-	return nil
-}
-
-func (mU *mockUserService) Update(userID int, user api.User) error {
-	_, exists := mU.userRepo[userID]
-
-	if !exists {
-		return &api.ErrNonExistentResource{}
-	}
-
-	return nil
-}
 
 type mockBiddingService struct {
 	biddingRepo map[int]api.Bidding
