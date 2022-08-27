16ab108a22e9db5324af9832ebbcdee443397871
Chester Asuncion <asuncionmcv@gmail.com>
2022-08-27 21:10:22 +0800

    Move mockBidService to bid handlers test

--- pkg/app/bid_handlers_test.go
+++ pkg/app/bid_handlers_test.go
@@ -3,6 +3,7 @@ package app_test
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"online-bidding-system/pkg/api"
@@ -11,6 +12,46 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
+
+type mockBidService struct{}
+
+func (mBid *mockBidService) CreateBid(bidderID, biddingID, bidAmount int) (bidID int, err error) {
+	// bidder does not exist
+	if bidderID > 1 {
+		err = errors.New(`Bidder does not exist`)
+		return
+	}
+
+	// bidding does not exist
+	if biddingID > 1 {
+		err = errors.New(`Bidding does not exist`)
+		return
+	}
+
+	// invalid amount
+	if bidAmount <= 100 {
+		err = errors.New(`The bid amount is lower than lowest allowable bid`)
+		return
+	}
+
+	// when all values pass return bidID 1
+	bidID = 1
+	return
+}
+
+func (mBid *mockBidService) UpdateBid(bidID, bidAmount int) (err error) {
+	if bidAmount <= 100 {
+		err = errors.New(`The bid amount is lower than lowest allowable bid`)
+		return
+	}
+
+	if bidID != 1 {
+		err = errors.New(`The bidder with the given bidding id does not exist`)
+		return
+	}
+
+	return nil
+}
 
 func SetUpBidHandlersTest() {
 	bid_service = &mockBidService{}
