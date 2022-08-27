3160eae19286c85e2f838e37b62089cf4d5327af
Chester Asuncion <asuncionmcv@gmail.com>
2022-08-27 20:51:15 +0800

    move mockBiddingService to bidding_handlers_test

--- pkg/app/mockServiceDefinitions_test.go
+++ pkg/app/mockServiceDefinitions_test.go
@@ -2,58 +2,7 @@ package app_test
 
 import (
 	"errors"
-	"online-bidding-system/pkg/api"
 )
-
-// this must be the same as that defined in api definition of the user.
-// this is made so that no api code is called in this test
-
-type mockBiddingService struct {
-	biddingRepo map[int]api.Bidding
-}
-
-var biddingRepo = map[int]api.Bidding{
-	0: {},
-	1: {},
-}
-
-func (mB *mockBiddingService) All() (biddings []api.Bidding, err error) {
-	return []api.Bidding{
-		biddingRepo[0],
-		biddingRepo[1],
-	}, nil
-}
-
-func (mB *mockBiddingService) Get(biddingID int) (bidding api.Bidding, err error) {
-	bidding, exists := mB.biddingRepo[biddingID]
-
-	if !exists {
-		return bidding, &api.ErrNonExistentResource{}
-	}
-
-	return bidding, nil
-}
-
-func (mB *mockBiddingService) Delete(biddingID int) (err error) {
-	// simulate a successful delete operation
-	_, exists := mB.biddingRepo[biddingID]
-
-	if !exists {
-		return &api.ErrNonExistentResource{}
-	}
-
-	return nil
-}
-
-func (mB *mockBiddingService) Update(biddingID int, bidding api.Bidding) (err error) {
-	_, exists := mB.biddingRepo[biddingID]
-
-	if !exists {
-		return &api.ErrNonExistentResource{}
-	}
-
-	return nil
-}
 
 type mockBidService struct{}
 
