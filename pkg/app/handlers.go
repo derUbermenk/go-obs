f54dba64117cb1dabf6dd138942aae5f472f320d
Chester Asuncion <asuncionmcv@gmail.com>
2022-09-13 20:37:45 +0800

    feature/LoginHandler: Add LoginRequest and AuthResponse structs

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -6,10 +6,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// reponse
+
 type GenericResponse struct {
 	Status  bool        `json:"status"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+}
+
+type AuthResponse struct {
+	AccessToken  string `json:"status"`
+	RefreshToken string `json:"message"`
+}
+
+// request
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func (s *Server) ApiStatus() gin.HandlerFunc {
