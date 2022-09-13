5eb9e52b94346667cffc217ca6c6ac01000fdc1c
Chester Asuncion <asuncionmcv@gmail.com>
2022-09-13 21:04:49 +0800

    feature/LoginHandler: Implement Handler Login

---
+++ pkg/app/auth_handlers.go
@@ -0,0 +1,65 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) LogIn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		loginRequest := &LoginRequest{}
+
+		// get the request body
+		err := c.ShouldBindJSON(loginRequest)
+		if err != nil {
+			c.JSON(
+				http.StatusBadRequest,
+				&GenericResponse{
+					Status:  false,
+					Message: err.Error(),
+				},
+			)
+
+			return
+		}
+
+		// login the user
+		err, accessToken, refreshToken := s.auth_service.LogIn(loginRequest.Email, loginRequest.Password)
+
+		if err.Error() == `Invalid credentials` {
+			c.JSON(
+				http.StatusUnauthorized,
+				&GenericResponse{
+					Status:  false,
+					Message: `Invalid credentials`,
+				},
+			)
+
+			return
+
+		} else if err != nil {
+			c.JSON(
+				http.StatusInternalServerError,
+				&GenericResponse{
+					Status:  false,
+					Message: ``,
+				},
+			)
+
+			return
+		}
+
+		c.JSON(
+			http.StatusOK,
+			&GenericResponse{
+				Status:  true,
+				Message: `Logged in`,
+				Data: &AuthResponse{
+					AccessToken:  accessToken,
+					RefreshToken: refreshToken,
+				},
+			},
+		)
+	}
+}
